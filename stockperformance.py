40342d82df2f90d83a337d3ceb0f1d6bf36426e3
aungthu16 <aprilboy.new@gmail.com>
2024-10-02 21:57:44 +0700

    Update stockperformance.py

--- stockperformance.py
+++ stockperformance.py
@@ -3,13 +3,18 @@ import yfinance as yf
 import requests
 from bs4 import BeautifulSoup
 import math
+import numpy
 import http.client
 import json
+import pandas as pd
+import plotly.graph_objects as go
 
 st.set_page_config(
     page_title='Stock Analysis Dashboard',
     page_icon=':chart_with_upwards_trend:',
 )
+
+#st.title("Stock Analysis Dashboard")
 
 @st.cache_data
 def get_stock_data(ticker, apiKey=None):
@@ -54,10 +59,10 @@ def get_stock_data(ticker, apiKey=None):
             st.warning(f"data request failed: {e}")
 
     lowercase_ticker = ticker.lower()
-    picture_url = "https://logos.stockanalysis.com/" + lowercase_ticker + ".svg"
+    picture_url = f'https://logos.stockanalysis.com/{lowercase_ticker}.svg'
 
     try:
-        sa_statistics_url = "https://stockanalysis.com/stocks/" + ticker + "/statistics/"
+        sa_statistics_url = f'https://stockanalysis.com/stocks/{ticker}/statistics/'
         sa_statistics_response = requests.get(sa_statistics_url)
         sa_statistics_soup = BeautifulSoup(sa_statistics_response.content, "html.parser")
 
@@ -81,28 +86,6 @@ def get_stock_data(ticker, apiKey=None):
             sa_analysts_count = sa_analysts_data.get("Analyst Count", "N/A")
     except Exception as e:
         st.warning(f"Failed to scrape data from StockAnalysis: {e}")
-
-    try:
-        sa_holding_table = sa_statistics_soup.find_all('table')[2]
-        sa_holding_data = {}
-        sa_share_outstanding = "N/A"
-        sa_owned_insiders = "N/A"
-        sa_owned_institutions = "N/A"
-
-        if sa_holding_table:
-            rows = sa_holding_table.find_all('tr')
-            for row in rows:
-                cols = row.find_all('td')
-                if len(cols) == 2:  
-                    key = cols[0].text.strip()
-                    value = cols[1].text.strip()
-                    sa_holding_data[key] = value
-
-            sa_share_outstanding = sa_holding_data.get("Shares Outstanding", "N/A")
-            sa_owned_insiders = sa_holding_data.get("Owned by Insiders (%)", "N/A")
-            sa_owned_institutions = sa_holding_data.get("Owned by Institutions (%)", "N/A")
-    except Exception as e:
-        st.warning(f"Failed to scrape holding data: {e}")
 
     stock = yf.Ticker(ticker)
     data = stock.history(period='max')
@@ -130,28 +113,45 @@ def get_stock_data(ticker, apiKey=None):
     forwardPe = stock.info.get('forwardPE', 'N/A')
     dividendYield = stock.info.get('dividendYield', 'N/A')
     payoutRatio = stock.info.get('payoutRatio', 'N/A')
+    sharesOutstanding = stock.info.get('sharesOutstanding', 'N/A')
+    try:
+        totalEsg = stock.sustainability.loc['totalEsg', 'esgScores']
+        enviScore = stock.sustainability.loc['environmentScore', 'esgScores']
+        socialScore = stock.sustainability.loc['socialScore', 'esgScores']
+        governScore = stock.sustainability.loc['governanceScore', 'esgScores']
+        percentile = stock.sustainability.loc['percentile', 'esgScores']
+        insiderPct = stock.major_holders.loc['insidersPercentHeld', 'Value']
+        institutionsPct = stock.major_holders.loc['institutionsPercentHeld', 'Value']
+    except (KeyError, AttributeError, IndexError) as e:
+        totalEsg = enviScore = socialScore = governScore = percentile = insiderPct = institutionsPct = "N/A"
 
     change_dollar = price - previous_close
     change_percent = (change_dollar / previous_close) * 100
 
-    return price, change_percent, change_dollar, beta, name, sector, industry, employee, marketCap, longProfile, eps, pegRatio, picture_url, country, sa_analysts_consensus, sa_analysts_targetprice, sa_analysts_count, yf_targetprice, yf_consensus, yf_analysts_count, website, peRatio, forwardPe, dividendYield, payoutRatio, sa_share_outstanding, sa_owned_insiders, sa_owned_institutions, performance_id, fair_value, fvDate, moat, moatDate, starRating
+    yf_mos = ((yf_targetprice - price)/yf_targetprice) * 100
+    sa_price_float = float(sa_analysts_targetprice.replace('$', ''))
+    sa_mos = ((sa_price_float - price)/sa_price_float) * 100
 
-'''
-# :chart_with_upwards_trend: Stock Analysis Dashboard
-
-Browse stock data and view the latest market trends.
-'''
+    return sharesOutstanding, institutionsPct, insiderPct, totalEsg, enviScore, socialScore, governScore, percentile, price, change_percent, change_dollar, beta, name, sector, industry, employee, marketCap, longProfile, eps, pegRatio, picture_url, country, sa_analysts_consensus, sa_analysts_targetprice, sa_analysts_count, yf_targetprice, yf_consensus, yf_analysts_count, website, peRatio, forwardPe, dividendYield, payoutRatio, performance_id, fair_value, fvDate, moat, moatDate, starRating, yf_mos, sa_mos
 
 ''
 ''
 
-ticker = st.text_input("Enter US Stock Ticker:", "AAPL")
+############### Inputs ###############
 
-apiKey = st.text_input("Enter your Morningstar RapidAPI Key (optional):", "")
+input_col1, input_col2 = st.columns([1, 3])
+with input_col1:
+    ticker = st.text_input("Enter US Stock Ticker:", "AAPL")
+with input_col2:
+    apiKey = st.text_input("Enter your RapidAPI Key (optional):", "")
+
+st.info('Data provided by Yahoo Finance, Morning Star, and StockAnalysis.com')
 
 if st.button("Submit"):
     try:
-        price, change_percent, change_dollar, beta, name, sector, industry, employee, marketCap, longProfile, eps, pegRatio, picture_url, country, sa_analysts_consensus, sa_analysts_targetprice, sa_analysts_count, yf_targetprice, yf_consensus, yf_analysts_count, website, peRatio, forwardPe, dividendYield, payoutRatio, sa_share_outstanding, sa_owned_insiders, sa_owned_institutions, performance_id, fair_value, fvDate, moat, moatDate, starRating = get_stock_data(ticker, apiKey if apiKey.strip() else None)
+        sharesOutstanding, institutionsPct, insiderPct, totalEsg, enviScore, socialScore, governScore, percentile, price, change_percent, change_dollar, beta, name, sector, industry, employee, marketCap, longProfile, eps, pegRatio, picture_url, country, sa_analysts_consensus, sa_analysts_targetprice, sa_analysts_count, yf_targetprice, yf_consensus, yf_analysts_count, website, peRatio, forwardPe, dividendYield, payoutRatio, performance_id, fair_value, fvDate, moat, moatDate, starRating, yf_mos, sa_mos = get_stock_data(ticker, apiKey if apiKey.strip() else None)
+    
+############### Profile ###############
 
         st.header(f'{name}', divider='gray')
 
@@ -164,7 +164,7 @@ if st.button("Submit"):
         col1, col2 = st.columns([2, 3])
         with col1:
             st.markdown(f"""
-            <div style='text-align: middle;'>
+            <div style='text-align: left;'>
                 <img src='{picture_url}' width='100'>
             </div>
             """, unsafe_allow_html=True)
@@ -184,17 +184,20 @@ if st.button("Submit"):
             """, unsafe_allow_html=True)
 
         h_cols = st.columns(3)
+        sharesOutstanding_value = 'N/A' if sharesOutstanding == 'N/A' else f'{sharesOutstanding/1000000000:,.2f}B'
         h_cols[0].metric(
             label='Shares Outstanding',
-            value=sa_share_outstanding
+            value=sharesOutstanding_value
         )
+        insiderPct_value = 'N/A' if insiderPct == 'N/A' else f'{insiderPct*100:,.2f}%'
         h_cols[1].metric(
             label='Owned by Insiders',
-            value=sa_owned_insiders
+            value=insiderPct_value
         )
+        institutionsPct_value = 'N/A' if institutionsPct == 'N/A' else f'{institutionsPct*100:,.2f}%'
         h_cols[2].metric(
             label='Owned by Institutions',
-            value=sa_owned_institutions
+            value=institutionsPct_value
         )
 
         st.markdown(f"<div style='text-align: justify;'>{longProfile}</div>", unsafe_allow_html=True)
@@ -202,93 +205,196 @@ if st.button("Submit"):
         ''
         ''
 
-        st.subheader('Stock Performance', divider='gray')
+############### Tabs ###############
 
-        cols = st.columns(4)
+        overview_data, comparison_data, statements_data, sustainability_data, news_data, technicalAnalysis_data = st.tabs (["Overview","Comparisons","Statement","Sustainability","Top 10 news", "Technical Analysis"])
 
-        cols[0].metric(
-            label=f'{ticker.upper()} Current Price',
-            value=f'${price:,.2f}',
-            delta=f'{change_dollar:,.2f} ({change_percent:.2f}%)',
-            delta_color='normal' 
-        )
+############### Overview Data ###############
 
-        eps_value = 'N/A' if eps == 'N/A' else f'{eps:,.2f}'
-        cols[1].metric(
-            label='EPS (ttm)',
-            value=eps_value
-        )
+        with overview_data:
 
-        pegRatio_value = 'N/A' if pegRatio == 'N/A' else f'{pegRatio:,.2f}'
-        cols[2].metric(
-            label='PEG Ratio',
-            value=pegRatio_value
-        )
+            #Stock Performance
+            st.subheader('Stock Performance', divider='gray')
+            cols = st.columns(4)
+            cols[0].metric(
+                label=f'{ticker.upper()} Current Price',
+                value=f'${price:,.2f}',
+                delta=f'{change_dollar:,.2f} ({change_percent:.2f}%)',
+                delta_color='normal' 
+            )
+            eps_value = 'N/A' if eps == 'N/A' else f'{eps:,.2f}'
+            cols[1].metric(
+                label='EPS (ttm)',
+                value=eps_value
+            )
+            pegRatio_value = 'N/A' if pegRatio == 'N/A' else f'{pegRatio:,.2f}'
+            cols[2].metric(
+                label='PEG Ratio',
+                value=pegRatio_value
+            )
+            beta_value = 'N/A' if beta == 'N/A' else f'{beta:.2f}'
+            cols[3].metric(
+                label='Beta',
+                value=beta_value
+            )
 
-        beta_value = 'N/A' if beta == 'N/A' else f'{beta:.2f}'
-        cols[3].metric(
-            label='Beta',
-            value=beta_value
-        )
+            cols1 = st.columns(4)
+            pe_value = 'N/A' if peRatio == 'N/A' else f'{peRatio:.2f}'
+            cols1[0].metric(
+                label='PE Ratio',
+                value=pe_value
+            )
+            forwardPe_value = 'N/A' if forwardPe == 'N/A' else f'{forwardPe:.2f}'
+            cols1[1].metric(
+                label='Forward PE',
+                value=forwardPe_value
+            )
+            dividendyield_value = 'N/A' if dividendYield == 'N/A' else f'{dividendYield*100:.2f}%'
+            cols1[2].metric(
+                label='Dividend Yield',
+                value=dividendyield_value
+            )
+            payoutRatio_value = 'N/A' if payoutRatio == 'N/A' else f'{payoutRatio:.2f}'
+            cols1[3].metric(
+                label='Payout Ratio',
+                value=payoutRatio_value
+            )
 
-        cols1 = st.columns(4)
+            #Morning Star Research
+            st.subheader('Morningstar Research', divider='gray')
+            st.caption("This section only works with RapidAPI key.")
+            mscol = st.columns(3)
+            mscol[0].metric(
+                label='Economic Moat',
+                value=moat,
+            )
+            fair_value_mos ='N/A' if fair_value =='N/A' else f'{((fair_value - price)/fair_value) * 100:.2f}%'
+            mscol[1].metric(
+                label='Fair Value',
+                value=fair_value,
+                delta=fair_value_mos,
+                delta_color='normal' 
+            )
+            mscol[2].metric(
+                label='Rating',
+                value=starRating
+            )
+            st.write('Moat Assessment Date: ' f'{moatDate}')
+            st.write('Fair Value Assessment Date: ' f'{fvDate}')
 
-        pe_value = 'N/A' if peRatio == 'N/A' else f'{peRatio:.2f}'
-        cols1[0].metric(
-            label='PE Ratio',
-            value=pe_value
-        )
+            #Analysts Ratings
+            st.subheader('Analysts Ratings', divider='gray')
+            col1, col2 = st.columns([3, 3])
+            sa_mos_value = 'N/A' if sa_mos == 'N/A' else f'{sa_mos:.2f}%'
+            with col1:
+                st.write('stockanalysis.com')
+                st.write(f'Price Target: {sa_analysts_targetprice}')
+                st.write(f'Difference %: {sa_mos_value}')
+                st.write(f'Analyst Consensus: {sa_analysts_consensus}')
+                st.write(f'Analyst Count: {sa_analysts_count}')
 
-        forwardPe_value = 'N/A' if forwardPe == 'N/A' else f'{forwardPe:.2f}'
-        cols1[1].metric(
-            label='Forward PE',
-            value=forwardPe_value
-        )
+            yf_targetprice_value = 'N/A' if yf_targetprice == 'N/A' else f'${yf_targetprice}'
+            yf_mos_value = 'N/A' if yf_mos == 'N/A' else f'{yf_mos:.2f}%'
+            with col2:
+                st.write('Yahoo Finance')
+                st.write(f'Price Target: {yf_targetprice_value}')
+                st.write(f'Difference %: {yf_mos_value}')
+                st.write(f'Analyst Consensus: {yf_consensus}')
+                st.write(f'Analyst Count: {yf_analysts_count}')
 
-        dividendyield_value = 'N/A' if dividendYield == 'N/A' else f'{dividendYield*100:.2f}%'
-        cols1[2].metric(
-            label='Dividend Yield',
-            value=dividendyield_value
-        )
+############### Statements ###############
 
-        payoutRatio_value = 'N/A' if payoutRatio == 'N/A' else f'{payoutRatio:.2f}'
-        cols1[3].metric(
-            label='Payout Ratio',
-            value=payoutRatio_value
-        )
+        with comparison_data:
+            st.write("Comparisons")
 
-        st.subheader('Quantitative Scores', divider='gray')
+############### Statements ###############
 
-        st.write(f'Fair Value: {fair_value}')
-        st.write(f'Fair Value Date: {fvDate}')
-        st.write(f'Economic Moat: {moat}')
-        st.write(f'Moat Date: {moatDate}')
-        st.write(f'Star Rating: {starRating}')
+        with statements_data:
+            st.write("Statements")
 
-        st.subheader('Analysts Ratings', divider='gray')
+############### Technical Analysis Data ###############
 
-        col1, col2 = st.columns([3, 3])
-        with col1:
-            st.markdown(f"""
-            <table>
-                <tr><td colspan="2" style="text-align: center;"><strong>Stockanalysis.com</strong></td></tr>
-                <tr><td><strong>Price Target</strong></td><td>{sa_analysts_targetprice}</td></tr>
-                <tr><td><strong>Analyst Consensus</strong></td><td>{sa_analysts_consensus}</td></tr>
-                <tr><td><strong>Analyst Count</strong></td><td>{sa_analysts_count}</td></tr>
-            </table>
-            """, unsafe_allow_html=True)
+        with technicalAnalysis_data:
+            st.subheader("Price Data", divider ='gray')
+            """
+            col1, col2 = st.columns([3, 3])
+            with col1:    
+                start_date = st.date_input('Start Date')
+            with col2:
+                end_date = st.date_input('End date')
+            data = yf.download(ticker, start=start_date,end=end_date)
+            fig = px.line(data, x = data.index, y=data['Adj close'],title = ticker)
+            st.plotly_chart(fig) 
+            """
+            #df = pd.DataFrame()
+            #ind_list = df.ta.indicators(as_list=True)
+            #st.write(ind_list)
+        
+############### Sustainability Data ###############
 
-        yf_targetprice_value = 'N/A' if yf_targetprice == 'N/A' else f'${yf_targetprice}'
+        with sustainability_data:
+            st.subheader("Sustainability", divider = 'gray')
+            #st.caption("This section shows the ESG risk ratings of '" + name + "' .")
 
-        with col2:
-            st.markdown(f"""
-            <table>
-                <tr><td colspan="2" style="text-align: center;"><strong>Yahoo Finance</strong></td></tr>
-                <tr><td><strong>Price Target</strong></td><td>{yf_targetprice_value}</td></tr>
-                <tr><td><strong>Analyst Consensus</strong></td><td>{yf_consensus}</td></tr>
-                <tr><td><strong>Analyst Count</strong></td><td>{yf_analysts_count}</td></tr>
-            </table>
-            """, unsafe_allow_html=True)
+            #Gauge Plot
+            def plot_gauge():
+                totalEsg_value = 0.00 if totalEsg == 'N/A' else totalEsg
+                max_value = 100
+                gauge = go.Figure(go.Indicator(
+                    mode="gauge+number",
+                    value=totalEsg_value,
+                    title={'text': "Total ESG Risk"},
+                    gauge={
+                        'axis': {'range': [None, max_value]},
+                        'bar': {'color': "blue"},
+                        'steps': [
+                            {'range': [0, max_value * 0.25], 'color': "lightgray"},
+                            {'range': [max_value * 0.25, max_value * 0.5], 'color': "yellow"},
+                            {'range': [max_value * 0.5, max_value * 0.75], 'color': "orange"},
+                            {'range': [max_value * 0.75, max_value], 'color': "red"},
+                        ],
+                    }
+                ))
+                gauge.update_layout(
+                    autosize=False,
+                    width=300,  
+                    height=200, 
+                    margin={'l': 50, 'r': 50, 't': 10, 'b': 0} 
+                )
+                st.plotly_chart(gauge)
+            left_column, middle_column, right_column = st.columns([1, 3, 1])
+            with middle_column:
+                plot_gauge()
+            
+            #Risk Scores
+            esgcol1 = st.columns(3)
+            esgcol1[0].metric(
+                label='Environmental Risk',
+                value=enviScore
+            )
+            esgcol1[1].metric(
+                label='Social Risk',
+                value=socialScore
+            )
+            esgcol1[2].metric(
+                label='Governance Risk',
+                value=governScore
+            )
+
+            #Descriptions
+            st.caption("Notes:")
+            st.write("Total ESG Risk: Companies with ESG scores closer to 100 are considered to have significant ESG-related risks and challenges that could potentially harm their long-term sustainability.")
+            st.write("Environmental Risk: This reflects the company’s impact on the environment. e.g. carbon emissions, waste management, energy efficiency.")
+            st.write("Social Risk: This measures the company’s relationships with employees, suppliers, customers, and the community. e.g. human rights, labor practices, diversity, and community engagement.")
+            st.write("Governance Risk: this focuses on the company’s leadership, audit practices, internal controls, and shareholder rights. e.g. transparent financial reporting and strong board oversight.")
+
+############### News Data ###############
+
+        with news_data:
+            st.subheader("News", divider = 'gray')
+            st.write(f'{name}')
+            st.write(f'{ticker}')
+            ''
 
     except Exception as e:
         st.error(f"Failed to fetch data. {str(e)}")
@@ -296,5 +402,3 @@ if st.button("Submit"):
 ''
 ''
 ''
-
-st.info('Data provided by Yahoo Finance, Morning Star, and StockAnalysis.com')
