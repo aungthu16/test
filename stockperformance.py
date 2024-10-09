1e8f417dd135351332f8b50fbaca4c6b6c666483
aungthu16 <aprilboy.new@gmail.com>
2024-10-09 09:58:37 +0700

    Update stockperformance.py

--- stockperformance.py
+++ stockperformance.py
@@ -232,6 +232,7 @@ if st.button("Submit"):
         col1, col2 = st.columns([2, 3])
         with col1:
             st.image(picture_url, use_column_width=True)
+            ''
             # st.markdown(f"""
             # <div style='text-align: left;'>
             #     <img src='{picture_url}' width='100'>
